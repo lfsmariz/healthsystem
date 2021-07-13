12207bde187306ecb897a4278acd97a18db205a4
Anna Albuquerque <anna.b.albuquerque@gmail.com>
2021-07-13 03:15:41 -0300

    Adds Admin fix(Entity): modifies Admin entity feature(Controller): Creates AdminController feature(DTO): Creates AdminDTO feature(Form): Creates AdminForm feature(Service): Creates AdminService

---
+++ src/main/java/edu/ufrn/pds/healthsystem/form/AdminForm.java
@@ -0,0 +1,12 @@
+package edu.ufrn.pds.healthsystem.form;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class AdminForm {
+    String name;
+}
