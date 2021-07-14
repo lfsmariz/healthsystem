f32cbac3cf5650bb9c22086c99c600cb4d63841e
renanamr <renanrochamorais@outlook.com>
2021-07-14 19:43:31 -0300

    New

--- src/main/java/edu/ufrn/pds/healthsystem/repository/AchievementRepository.java
+++ src/main/java/edu/ufrn/pds/healthsystem/repository/AchievementRepository.java
@@ -11,6 +11,7 @@ import java.util.Map;
 
 @Repository
 public interface AchievementRepository extends JpaRepository<Achievement,Long> {
+
     @Query("select a.name as name, a.points as points, a.id as id, b.name as board_name from #{#entityName} a join a.board b where b.id = ?1")
     List<Map> findByIdBoardAndIdUser(Long idBoard, Long idUser);
 }
