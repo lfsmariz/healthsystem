9f39d5b5c5ecbe231f2ed318da6ae0848cc96d37
Anna Albuquerque <anna.b.albuquerque@gmail.com>
2021-07-15 06:20:19 -0300

    feat(player_boards): player's board visualization feat(DTO): Creates BoardsByPlayersDTO feat(DTO): Adds constructor BoardDTO(Board board) feat(Controller): Adds getBoards feat(Service): Adds getBoards

--- src/main/java/edu/ufrn/pds/healthsystem/dto/BoardDTO.java
+++ src/main/java/edu/ufrn/pds/healthsystem/dto/BoardDTO.java
@@ -1,15 +1,23 @@
 package edu.ufrn.pds.healthsystem.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import edu.ufrn.pds.healthsystem.entity.Board;
+import lombok.*;
 
 import java.time.LocalDate;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class BoardDTO {
     private String name;
     private Long id_admin;
     private Long id_board;
     private LocalDate date_end;
+
+    public BoardDTO(Board board){
+        this.name = board.getName();
+        this.id_admin = board.getAdmin().getId();
+        this.id_board = board.getId();
+        this.date_end = board.getDateEnd();
+    }
 }
--- src/main/java/edu/ufrn/pds/healthsystem/service/PlayerService.java
+++ src/main/java/edu/ufrn/pds/healthsystem/service/PlayerService.java
@@ -1,11 +1,16 @@
 package edu.ufrn.pds.healthsystem.service;
 
+import edu.ufrn.pds.healthsystem.dto.BoardDTO;
+import edu.ufrn.pds.healthsystem.dto.BoardsByPlayersDTO;
 import edu.ufrn.pds.healthsystem.dto.PlayerDTO;
 import edu.ufrn.pds.healthsystem.entity.Player;
 import edu.ufrn.pds.healthsystem.form.PlayerForm;
 import edu.ufrn.pds.healthsystem.repository.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class PlayerService {
@@ -31,5 +36,13 @@ public class PlayerService {
         Player p = playerRepository.save(player);
 
         return new PlayerDTO(p);
+    }
+
+    public BoardsByPlayersDTO getBoards(Long idPlayer){
+       Set<BoardDTO> boards;
+       Player player = playerRepository.findById(idPlayer).orElseThrow(RuntimeException::new);
+       boards =  player.getBoards().stream().map(BoardDTO::new).collect(Collectors.toSet());
+
+       return new BoardsByPlayersDTO(boards);
     }
 }
