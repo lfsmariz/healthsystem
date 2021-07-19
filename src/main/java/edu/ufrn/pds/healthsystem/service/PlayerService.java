f426617bd7bd5ba6f62577f811fe4a4bc6a0b002
Luiz Filipe S Mariz <lfsmariz@gmail.com>
2021-07-19 17:55:11 -0300

    feat(populate): populate sql with data.sql feat(sprint3): view all redeemed achievements and redeem achievements

--- src/main/java/edu/ufrn/pds/healthsystem/service/PlayerService.java
+++ src/main/java/edu/ufrn/pds/healthsystem/service/PlayerService.java
@@ -1,10 +1,11 @@
 package edu.ufrn.pds.healthsystem.service;
 
-import edu.ufrn.pds.healthsystem.dto.BoardDTO;
-import edu.ufrn.pds.healthsystem.dto.BoardsByPlayersDTO;
-import edu.ufrn.pds.healthsystem.dto.PlayerDTO;
+import edu.ufrn.pds.healthsystem.dto.*;
+import edu.ufrn.pds.healthsystem.entity.Achievement;
 import edu.ufrn.pds.healthsystem.entity.Player;
+import edu.ufrn.pds.healthsystem.form.AchievementCompleteForm;
 import edu.ufrn.pds.healthsystem.form.PlayerForm;
+import edu.ufrn.pds.healthsystem.repository.AchievementRepository;
 import edu.ufrn.pds.healthsystem.repository.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,10 +17,12 @@ import java.util.stream.Collectors;
 public class PlayerService {
 
     private final PlayerRepository playerRepository;
+    private final AchievementRepository achievementRepository;
 
     @Autowired
-    PlayerService(PlayerRepository playerRepository){
+    PlayerService(PlayerRepository playerRepository, AchievementRepository achievementRepository){
         this.playerRepository = playerRepository;
+        this.achievementRepository = achievementRepository;
     }
 
     public PlayerDTO create(PlayerForm playerForm){
@@ -44,5 +47,30 @@ public class PlayerService {
        boards =  player.getBoards().stream().map(BoardDTO::new).collect(Collectors.toSet());
 
        return new BoardsByPlayersDTO(boards);
+    }
+
+    public AchievementCompleteDTO redeemAchievement(AchievementCompleteForm form) {
+        Player player = playerRepository.findById(form.getId_user()).orElseThrow(RuntimeException::new);
+        Achievement achievement = achievementRepository.findById(form.getId_achievement()).orElseThrow(RuntimeException::new);
+
+        boolean playerContainAchiev = player.getAchievements().contains(achievement);
+        if(player.getPoints() < achievement.getPoints() || playerContainAchiev){
+            throw new RuntimeException("sem pontos suficientes");
+        }
+
+        player.setPoints(player.getPoints() - achievement.getPoints());
+        achievement.getPlayers().add(player);
+
+        achievementRepository.save(achievement);
+
+        return new AchievementCompleteDTO(achievement);
+    }
+
+    public AchievementsByPlayer getMyAchievements(Long id_player) {
+        Player player = playerRepository.findById(id_player).orElseThrow(RuntimeException::new);
+
+        Set<Achievement> achivementSet = player.getAchievements();
+
+        return new AchievementsByPlayer(achivementSet);
     }
 }
